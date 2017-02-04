d0d9646a1e7df1d7bdbd9491610734d25f6eef8d
Jerke Combee <jerke1988@gmail.com>
2017-02-05 00:06:13 +0100

    Added autocompletion to the load function

--- src/load_controller.sh
+++ src/load_controller.sh
@@ -4,4 +4,18 @@ function _cdaLoadController() {
   _cdaAliases=()
   _cdaAliasPaths=()
   . ~/.cda_list 2>&1
+
+  alias cda=". /c/bin/cda/cda.sh"
+  _cda()
+  {
+    _cda_commands="${_cdaCommands[@]} ${_cdaAliases[@]}"
+
+    local cur prev
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=( $(compgen -W "${_cda_commands}" -- ${cur}) )
+
+    return 0
+  }
+  complete -o nospace -F _cda cda
 }
--- tests/run.sh
+++ tests/run.sh
@@ -2,11 +2,15 @@
 
 cd `dirname $0`
 
+mv ~/.cda_list ~/.cda_list_temp
+
 echo Start the tests
 
 for file in `ls | grep test_`
 do
   sh ./$file $1
 done
+
+mv ~/.cda_list_temp ~/.cda_list
 
 echo End of testing
