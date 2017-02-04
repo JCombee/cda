66e84cab53d0cdfaddd73daa1c1a011b0a013d31
Jerke Combee <jerke1988@gmail.com>
2017-02-04 20:58:37 +0100

    updated set to not append already existing records but overwrite

--- src/set_controller.sh
+++ src/set_controller.sh
@@ -1,10 +1,23 @@
 _cdaController set _cdaSetController
 
-_cdaAliases=()
-
 function _cdaSetController() {
   _cdaNewAlias=$2
   _cdaNewPath=$3
-  _cdaAliases+=($_cdaNewAlias)
+  _counter=0
+  _cont=true
+
+  for _cdaAlias in "${_cdaAliases[@]}"
+  do
+    if [ $_cdaAlias == $_cdaNewAlias ]; then
+      _cdaAliases[$_counter]=$_cdaNewAlias
+      _cont=false
+    fi
+    _counter=`expr $_counter + 1`
+  done
+
+  if $_cont; then
+    _cdaAliases+=($_cdaNewAlias)
+  fi
+
   echo "Alias \"$_cdaNewAlias\" is set to \"$_cdaNewPath\""
 }
