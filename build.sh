d4129df7ea563b1d43142230f0bf4adb5123ded3
Jerke Combee <jerke1988@gmail.com>
2017-02-05 16:39:15 +0100

    Made build run indepent of location

--- build.sh
+++ build.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# bootstrapping the application
-
-. `pwd`/bootstrap.sh
-
-# removing the old build if there is one
-touch `pwd`/cda.sh
-rm `pwd`/cda.sh
-
-# build the files
-touch `pwd`/cda.sh
-for file in "${_cdaFiles[@]}"
-do
-  cat ${file} >> `pwd`/cda.sh
-done
+if [ "$_cdaSourcePath" == "" ]; then
+  _cdaSourcePath="${BASH_SOURCE[0]}"
+  _cdaSourcePathDir=`dirname "$_cdaSourcePath"`
+fi
 
 # run the tests before the build will be trigered
-sh `pwd`/tests/run.sh `pwd`/cda.sh
+sh $_cdaSourcePathDir/tests/run.sh $_cdaSourcePathDir/cda.sh
 
 if test $? == false; then
-  echo The test have failed!
+  echo The test has failed!
   return false
 fi
+
+# bootstrapping the application
+source $_cdaSourcePathDir/bootstrap.sh
+
+# removing the old build if there is one
+rm $_cdaSourcePathDir/build -rf 2>&1
+
+# build the files
+mkdir $_cdaSourcePathDir/build
+touch $_cdaSourcePathDir/build/cda.sh
+
+for file in "${_cdaFiles[@]}"
+do
+  cat ${file} >> $_cdaSourcePathDir/build/cda.sh
+done
